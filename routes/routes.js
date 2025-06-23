c55ca005b0a87738afe7b5418130c6f707e5753b
Santhosh D <santhosh.webd679@gmail.com>
2025-06-23 15:26:21 +0530

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,14 +2,22 @@ const modelfile = require("../model/model");
 const express = require("express")
 const app = express.Router();
 
+const setCorsHeaders = (res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://littlereviewmain.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+};
+
 app.post('/push',async(req,res)=>{
+    setCorsHeaders(res);
     const browserdata = req.body
     const pushingdata = await new modelfile(browserdata);
     pushingdata.save();
     res.json("data added successfully")
 })
 
-app.get('/get/',async(req,res)=>{
+app.get('/get',async(req,res)=>{
+    setCorsHeaders(res);
     const dbdatas = await modelfile.find();
     // console.log(dbdatas);
     res.json(dbdatas)
